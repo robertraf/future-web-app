de64185f07cf88564d167ca645300a030e089ee9
Roberto Ramírez <roberto.ramirez@premper.com>
2021-01-30 19:19:47 -0600

    :rocket: First Commit & Typescript setup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Future Web App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
