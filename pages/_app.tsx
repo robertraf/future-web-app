e74050c252eb84b7a2a3874bd4d3982c23afa3d9
Roberto Ramírez <roberto.ramirez@premper.com>
2021-01-30 19:43:53 -0600

    :rocket: Tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 import type { AppProps /*, AppContext */ } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,29 @@ const Home = () => {
         <title>Future Web App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="max-w-md mx-auto">
+              <div>
+                <img src="/vercel.svg" className="h-7 sm:h-8" />
+              </div>
+              <div className="divide-y divide-gray-200">
+                <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                  <p>Want to dig deeper into Future Web App?</p>
+                  <p>
+                    <a href="#" className="text-cyan-600 hover:text-cyan-700">
+                      {" "}
+                      Read the docs &rarr;{" "}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
