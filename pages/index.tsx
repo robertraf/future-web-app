fd914f8a39e1b4cab145a410381d76a837f4bab1
Roberto Ramírez <robertrafyt2007@gmail.com>
2021-04-02 20:22:39 -0600

    :construction: WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,58 @@
 import Head from "next/head";
 import Image from "next/image";
+import { supabase } from "@/lib/initSupabase";
+import { Auth } from "@supabase/ui";
 
 const Home = () => {
+  const { user } = Auth.useUser();
+
+  console.log(`user`, user);
   return (
-    <div>
+    <>
       <Head>
         <title>Future Web App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-            <div className="max-w-md mx-auto">
-              <div className="h-7 sm:h-8 relative">
-                <Image src="/vercel.svg" alt="company logo" layout="fill" />
-              </div>
-              <div className="divide-y divide-gray-200">
-                <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-                  <p>Want to dig deeper into Future Web App?</p>
-                  <p>
-                    <a href="#" className="text-cyan-600 hover:text-cyan-700">
-                      {" "}
-                      Read the docs &rarr;{" "}
-                    </a>
-                  </p>
+      <div className="w-full h-full bg-gray-300">
+        {!user ? (
+          <div className="w-full h-full flex justify-center items-center p-4">
+            <Auth
+              supabaseClient={supabase}
+              providers={["github"]}
+              socialLayout="horizontal"
+              socialButtonSize="xlarge"
+            />
+          </div>
+        ) : (
+          <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+              <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+              <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                <div className="max-w-md mx-auto">
+                  <div className="h-7 sm:h-8 relative">
+                    <Image src="/vercel.svg" alt="company logo" layout="fill" />
+                  </div>
+                  <div className="divide-y divide-gray-200">
+                    <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                      <p>Want to dig deeper into Future Web App?</p>
+                      <p>
+                        <a
+                          href="#"
+                          className="text-cyan-600 hover:text-cyan-700"
+                        >
+                          {" "}
+                          Read the docs &rarr;{" "}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
