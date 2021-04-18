394857aba57ab7d794f5562aeb224e88ab772fe1
Roberto Ramírez <robertrafyt2007@gmail.com>
2021-04-18 16:39:26 -0600

    :construction: WIP

--- components/PageList.tsx
+++ components/PageList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { slugify } from "@/utils/slugify";
+import PageCard from "@/components/PageCard";
+import UserPageCard from "@/components/UserPageCard";
 
 import { supabase } from "@/lib/initSupabase";
 
@@ -102,36 +103,13 @@ const PageList = ({ user }) => {
           </div>
         </div>
         <div className="flex flex-wrap -m-2">
-          {pages.map((page) => (
-            <div key={page.id} className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <Link href={`/page/${page.slug}`}>
-                <a>
-                  <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="avatar"
-                      className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src={
-                        page?.users?.avatar_url
-                          ? page.users.avatar_url
-                          : "https://dummyimage.com/80x80"
-                      }
-                      loading="lazy"
-                    />
-                    <div className="flex-grow">
-                      <h2 className="text-gray-900 title-font font-medium">
-                        {page.title}
-                      </h2>
-                      <p className="text-gray-500">{`by ${
-                        page?.users?.full_name
-                          ? page.users.full_name
-                          : "Secret User"
-                      }`}</p>
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </div>
-          ))}
+          {pages.map((page) =>
+            pageFilter === "1" ? (
+              <PageCard page={page} />
+            ) : (
+              <UserPageCard page={page} />
+            )
+          )}
         </div>
       </div>
     </section>
