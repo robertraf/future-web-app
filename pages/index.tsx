d4675c18d2f38433291cda46ac24fe6c1cac8e9f
Roberto Ramírez <robertrafyt2007@gmail.com>
2021-04-18 14:05:15 -0600

    :rocket: Feature - Adding SSR with auth cookie
    :construction: WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import useSWR from "swr";
 import Head from "next/head";
+// import { fetcher } from "@/utils/fetcher";
 import PageList from "@/components/PageList";
 
 import { supabase } from "@/lib/initSupabase";
 import { Auth } from "@supabase/ui";
 
 const Home = () => {
-  const { user } = Auth.useUser();
+  const {
+    user,
+    // session
+  } = Auth.useUser();
+
+  // const { data, error } = useSWR(
+  //   session ? ["/api/getUser", session.access_token] : null,
+  //   fetcher
+  // );
+
+  // const [authView, setAuthView] = useState("sign_in");
+
   const [showDropdown, setShowDropdown] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        // if (event === "PASSWORD_RECOVERY") setAuthView("forgotten_password");
+        // if (event === "USER_UPDATED")
+        //   setTimeout(() => setAuthView("sign_in"), 1000);
+        // Send session to /api/auth route to set the auth cookie.
+        // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
+        fetch("/api/auth", {
+          method: "POST",
+          headers: new Headers({ "Content-Type": "application/json" }),
+          credentials: "same-origin",
+          body: JSON.stringify({ event, session }),
+        }).then((res) => res.json());
+      }
+    );
+
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
 
   return (
     <>
--- pages/page/[slug].tsx
+++ pages/page/[slug].tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { GetServerSideProps } from "next";
 import { useCMS, withTina } from "tinacms";
 import TinaForm from "@/components/TinaForm";
@@ -8,39 +7,28 @@ import { Auth } from "@supabase/ui";
 
 export const getServerSideProps: GetServerSideProps = async ({
   params: { slug },
+  // req,
 }) => {
+  //const { user } = await supabase.auth.api.getUserByCookie(req);
+
+  const { data: page, error } = await supabase
+    .from("pages")
+    .select("*")
+    .eq("slug", slug);
+
+  if (error) console.log("error", error);
+
   return {
     props: {
-      slug,
+      page,
     },
   };
 };
 
-const Page = ({ slug }) => {
-  const [page, setPage] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Page = ({ page }) => {
   const cms = useCMS();
 
   const { user } = Auth.useUser();
-
-  useEffect(() => {
-    fetchPage();
-  }, []);
-
-  const fetchPage = async () => {
-    setLoading(true);
-    let { data: page, error } = await supabase
-      .from("pages")
-      .select("*")
-      .eq("slug", slug);
-
-    if (error) console.log("error", error);
-    else setPage(page);
-    setLoading(false);
-  };
-
-  if (loading) return <div>Loading...</div>;
 
   if (!page?.length) return <div>Page not found</div>;
 
